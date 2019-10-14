5567b2210d81d8daac63b83e147ef348d29fd62b
zhangxiaolin <zhangxiaolin@easylabplus.com>
2019-10-14 15:09:17 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -24,7 +24,6 @@ Vue.use(Vuetify)
 //confirm
 import confirm from './public/confirm/confirm'
 Vue.prototype.$confirm = confirm;
-
 Vue.config.productionTip = false
 
 
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ module.exports={
   productionSourceMap: false,
   lintOnSave: process.env.NODE_ENV !== 'production', //禁用 eslint
   devServer: {
-    port: 8080, // 端口号
+    port: '8080', // 端口号  '0.0.0.0'=>有线上网址，自动获取本地IP
     host: 'localhost',
     https:false, // https:{type:Boolean}
     open: true, //配置自动启动浏览器
-    proxy:'http://locakhost:8082',
+    // proxy:'http://122.112.243.38:8891/',   //生产环境
+    proxy:'http://192.168.31.13:8082/',   //本地环境
   },
 }
